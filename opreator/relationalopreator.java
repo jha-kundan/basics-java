ea63918be004f39717da62d53061df8022bae89f
jha-kundan <kundankavyanshu1234@gmail.com>
2026-07-15 18:04:23 +0530

    Arithmetic

---
+++ opreator/relationalopreator.java
@@ -0,0 +1,12 @@
+/*
+Relational operators compare two values and return a boolean result.
+
+Example:
+
+int x = 10, y = 20;
+
+System.out.println(x < y);  // true
+System.out.println(x > y);  // false
+System.out.println(x == y); // false
+System.out.println(x != y); // true
+*/
