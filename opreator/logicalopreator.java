8b4f8da1e3710aa22d0eea2566dc74b27ec1adc0
jha-kundan <kundankavyanshu1234@gmail.com>
2026-07-15 18:06:46 +0530

    logicaloperator

---
+++ opreator/logicalopreator.java
@@ -0,0 +1,12 @@
+/*
+Logical operators evaluate conditions logically and return boolean results.
+
+Example:
+
+int age = 25;
+boolean hasLicense = true;
+
+if (age >= 18 && hasLicense) {
+    System.out.println("You can drive");
+}
+// Output: You can drive */
