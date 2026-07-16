33b1aa0472bb7aac68dc557354e2328cd5a342c0
jha-kundan <kundankavyanshu1234@gmail.com>
2026-07-16 13:39:36 +0530

    And Or operator

---
+++ opreator/andoropreator.java
@@ -0,0 +1,10 @@
+/*
+== checks equality between two operands.
+= assigns a value to a variable.
+Example:
+
+int a = 5;     // Assignment
+int b = 5;
+
+System.out.println(a == b); // Equality check, Output: true
+ */
