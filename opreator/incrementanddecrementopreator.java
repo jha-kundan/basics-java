ce3e54b2c907164435b39332c428e609bc242732
jha-kundan <kundankavyanshu1234@gmail.com>
2026-07-16 13:32:58 +0530

    increment and decrement opreators

---
+++ opreator/incrementanddecrementopreator.java
@@ -0,0 +1,10 @@
+/*
+Increment (++) increases a variable by one, while decrement (--) decreases a variable by one.
+
+Example:
+
+int num = 10;
+num++; // num becomes 11
+num--; // num becomes 10 again
+
+*/
